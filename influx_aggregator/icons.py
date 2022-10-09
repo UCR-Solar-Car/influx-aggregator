91d395166f9b65b44065e425b9b9366f1bb789b0
Divyank Shah <divyank.shah.2016@gmail.com>
2022-10-09 15:31:51 -0700

    pylinted and formatted

--- influx_aggregator/icons.py
+++ influx_aggregator/icons.py
@@ -14,38 +14,46 @@ class Icons:
 
     def set_left_indicator(self, l_indicator):
         self._left_indicator = l_indicator
+
     def get_left_indicator(self):
         return self._left_indicator
 
     def set_right_indicator(self, r_indicator):
         self._right_indicator = r_indicator
+
     def get_right_indicator(self):
         return self._right_indicator
 
     def set_day_lights(self, d_lights):
         self._day_lights = d_lights
+
     def get_day_lights(self):
         return self._day_lights
 
     def set_night_lights(self, n_lights):
         self._night_lights = n_lights
+
     def get_night_lights(self):
         return self._night_lights
 
     def set_horn(self, horn):
         self._horn = horn
+
     def get_horn(self):
         return self._horn
 
     def set_cruise_control(self, c_control):
         self._cruise_control = c_control
+
     def get_cruise_control(self):
         return self._cruise_control
 
     def set_gears(self, gear):
         self._gears = gear
+
     def get_gears(self):
         return self._gears
+
 
 class Gears(IntEnum):
     PARKING = 1
--- influx_aggregator/tire_pressure.py
+++ influx_aggregator/tire_pressure.py
@@ -8,21 +8,24 @@ class TirePressure:
 
     def set_front_left(self, front_left):
         self._front_left = front_left
+
     def get_front_left(self):
         return self._front_left
 
     def set_front_right(self, front_right):
         self._front_right = front_right
+
     def get_front_right(self):
         return self._front_right
 
     def set_back_left(self, back_left):
         self._back_left = back_left
+
     def get_back_left(self):
         return self._back_left
-    
+
     def set_back_right(self, back_right):
         self._back_right = back_right
+
     def get_back_right(self):
         return self._back_right
-        
