80e2c487a9d8ec9f4ae7f1b4cc4cf0cc588bcb56
avikovi <avikovi@gmail.com>
2022-10-09 15:05:35 -0700

    Added get/set methods for icons, motor, and tire_pressure

--- influx_aggregator/icons.py
+++ influx_aggregator/icons.py
@@ -4,14 +4,48 @@ from enum import IntEnum
 class Icons:
 
     def __init__(self):
-        self.left_indicator = False
-        self.right_indicator = False
-        self.day_lights = False
-        self.night_lights = False
-        self.horn = False
-        self.cruise_control = False
-        self.gears = Gears.PARKING
+        self._left_indicator = False
+        self._right_indicator = False
+        self._day_lights = False
+        self._night_lights = False
+        self._horn = False
+        self._cruise_control = False
+        self._gears = Gears.PARKING
 
+    def set_left_indicator(self, l_indicator):
+        self._left_indicator = l_indicator
+    def get_left_indicator(self):
+        return self._left_indicator
+
+    def set_right_indicator(self, r_indicator):
+        self._right_indicator = r_indicator
+    def get_right_indicator(self):
+        return self._right_indicator
+
+    def set_day_lights(self, d_lights):
+        self._day_lights = d_lights
+    def get_day_lights(self):
+        return self._day_lights
+
+    def set_night_lights(self, n_lights):
+        self._night_lights = n_lights
+    def get_night_lights(self):
+        return self._night_lights
+
+    def set_horn(self, horn):
+        self._horn = horn
+    def get_horn(self):
+        return self._horn
+
+    def set_cruise_control(self, c_control):
+        self._cruise_control = c_control
+    def get_cruise_control(self):
+        return self._cruise_control
+
+    def set_gears(self, gear):
+        self._gears = gear
+    def get_gears(self):
+        return self._gears
 
 class Gears(IntEnum):
     PARKING = 1
--- influx_aggregator/motor.py
+++ influx_aggregator/motor.py
@@ -1,8 +1,33 @@
 class Motor:
 
     def __init__(self):
-        self.motor_voltage = 0.0
-        self.motor_temperature = 0.0
-        self.motor_warning = False
-        self.speed = 0
-        self.distance = 0
+        self._motor_voltage = 0.0
+        self._motor_temperature = 0.0
+        self._motor_warning = False
+        self._speed = 0
+        self._distance = 0
+
+    def set_motor_voltage(self, m_volatge):
+        self._motor_voltage = m_volatge
+    def get_motor_voltage(self):
+        return self._motor_voltage
+
+    def set_motor_temperature(self, m_temp):
+        self._motor_temperature = m_temp
+    def get_motor_temperature(self):
+        return self._motor_temperature
+
+    def set_motor_warning(self, m_warning):
+        self._motor_warning = m_warning
+    def get_motor_warning(self):
+        return self._motor_warning
+
+    def set_speed(self, speed):
+        self._speed = speed
+    def get_speed(self):
+        return self._speed
+
+    def set_distance(self, dist):
+        self._distance = dist
+    def get_distance(self):
+        return self._distance
