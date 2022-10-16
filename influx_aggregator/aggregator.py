566c701e9dc33441de388f06c8379897b7ea154a
IseanB <Isean4B@gmail.com>
2022-10-16 16:16:36 -0700

    Fixed formatting/linting

--- influx_aggregator/aggregator.py
+++ influx_aggregator/aggregator.py
@@ -5,36 +5,38 @@ from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 from environs import Env
 
+
 def env_setup():
-    ENVIRONMENT = "dev"
+    enviorment = "dev"
 
     env = Env()
-    if ENVIRONMENT == "dev":
+    if enviorment == "dev":
         env.read_env(".env.dev", recurse=False)
-    elif ENVIRONMENT == "production":
+    elif enviorment == "production":
         env.read_env(".env.production", recurse=False)
 
-    TOKEN = env("TOKEN")
-    ORG = env("ORG")
-    BUCKET = env("BUCKET")
+    token = env("TOKEN")
+    org = env("ORG")
+    bucket = env("BUCKET")
 
-    return TOKEN, ORG, BUCKET
+    return token, org, bucket
+
 
 def aggregator():
     setup = env_setup()
 
-    TOKEN = setup[0]
-    ORG = setup[1]
-    BUCKET = setup[2]
+    token = setup[0]
+    org = setup[1]
+    bucket = setup[2]
 
-    with InfluxDBClient(url="http://localhost:8086", token=TOKEN,
-                        org=ORG) as client:
+    with InfluxDBClient(url="http://localhost:8086", token=token,
+                        org=org) as client:
         write_api = client.write_api(write_options=SYNCHRONOUS)
 
-        for i in range(1000):
+        for _ in range(1000):
             point = Point("testing").field("temperature",
-                                        float(random.randint(1, 100)))
-            write_api.write(BUCKET, ORG, point)
+                                           float(random.randint(1, 100)))
+            write_api.write(bucket, org, point)
             time.sleep(1)
 
         client.close()
