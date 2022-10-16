42c49a71202d43510f8ff75c03639ba717e47a0f
IseanB <Isean4B@gmail.com>
2022-10-16 16:09:15 -0700

    Reformated for build script

--- influx_aggregator/aggregator.py
+++ influx_aggregator/aggregator.py
@@ -5,26 +5,36 @@ from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 from environs import Env
 
-ENVIRONMENT = "dev"
+def env_setup():
+    ENVIRONMENT = "dev"
 
-env = Env()
-if ENVIRONMENT == "dev":
-    env.read_env(".env.dev", recurse=False)
-elif ENVIRONMENT == "production":
-    env.read_env(".env.production", recurse=False)
+    env = Env()
+    if ENVIRONMENT == "dev":
+        env.read_env(".env.dev", recurse=False)
+    elif ENVIRONMENT == "production":
+        env.read_env(".env.production", recurse=False)
 
-TOKEN = env("TOKEN")
-ORG = env("ORG")
-BUCKET = env("BUCKET")
+    TOKEN = env("TOKEN")
+    ORG = env("ORG")
+    BUCKET = env("BUCKET")
 
-with InfluxDBClient(url="http://localhost:8086", token=TOKEN,
-                    org=ORG) as client:
-    write_api = client.write_api(write_options=SYNCHRONOUS)
+    return TOKEN, ORG, BUCKET
 
-    for i in range(1000):
-        point = Point("testing").field("temperature",
-                                       float(random.randint(1, 100)))
-        write_api.write(BUCKET, ORG, point)
-        time.sleep(1)
+def aggregator():
+    setup = env_setup()
 
-    client.close()
+    TOKEN = setup[0]
+    ORG = setup[1]
+    BUCKET = setup[2]
+
+    with InfluxDBClient(url="http://localhost:8086", token=TOKEN,
+                        org=ORG) as client:
+        write_api = client.write_api(write_options=SYNCHRONOUS)
+
+        for i in range(1000):
+            point = Point("testing").field("temperature",
+                                        float(random.randint(1, 100)))
+            write_api.write(BUCKET, ORG, point)
+            time.sleep(1)
+
+        client.close()
