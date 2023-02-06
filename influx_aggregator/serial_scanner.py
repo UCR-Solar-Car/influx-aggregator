94c03941d8b5a1ba32870ca5b19110935617eee2
Mariam Golwalla <mgolw001@ucr.edu>
2023-02-05 16:14:19 -0800

    env variable for port and data filetering started

--- influx_aggregator/serial_scanner.py
+++ influx_aggregator/serial_scanner.py
@@ -3,13 +3,55 @@
 import serial
 import time
 
+from environs import Env
+#!/usr/bin/env python
+#pylint: disable=import-error
+import time
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
 
-print("PORT")
+ENVIRONMENT = "dev"
+
+env = Env()
+
+
+
+def env_setup():
+    env = Env()
+    env.read_env(".env-dev")
+    PORT = env("PORT")
+    print(PORT)
+    TOKEN = env("TOKEN")
+    ORG = env("ORG")
+    BUCKET = env("BUCKET")
+    return PORT, TOKEN, BUCKET, ORG
+
+
 i = 0
-    
-ser = serial.Serial("COM8", 9600, timeout = 5)
+setup = env_setup()
+port = setup[0]
+token = setup[1]
+bucket= setup[2]
+org = setup[3]
+
+ser = serial.Serial(port, 9600, timeout = 5)
 time.sleep(2)
 
 
-# while ser.inWaiting() > 0:
-#     print(ser.readline())
+while ser.inWaiting() > 0:
+    line = str(ser.readline())
+    line = line.strip("',/\\n")
+    line = line.strip("',/\\r")
+    line = line.strip("b")
+    line = line.strip("'")
+    line = line.strip("\'")
+    line = line.split(",")
+    
+    print(line)
+    # with InfluxDBClient(url="http://localhost:8086", token=token,
+    #                     org=org) as client:
+    #     write_api = client.write_api(write_options=SYNCHRONOUS)
+    #     write_api.write(
+    #             bucket, org,
+    #             Point(line[0]).field(
+    #                 line[1], line[2]))
