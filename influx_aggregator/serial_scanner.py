fb754ebf1aa9ffe8c298590edbb15570c10a7aaa
Mariam Golwalla <mgolw001@ucr.edu>
2023-02-12 16:25:20 -0800

    works, data from arduino->influx

--- influx_aggregator/serial_scanner.py
+++ influx_aggregator/serial_scanner.py
@@ -15,37 +15,32 @@ ENVIRONMENT = "dev"
 def env_setup():
     env = Env()
     env.read_env(".env-dev")
-    port_es = env("PORT")
-    #print(PORT)
-    token_es = env("TOKEN")
-    org_es = env("ORG")
-    bucket_es = env("BUCKET")
-    return port_es, token_es, org_es, bucket_es
+    port_env = env("PORT")
+    token_env = env("TOKEN")
+    org_env = env("ORG")
+    bucket_env= env("BUCKET")
+    return port_env, token_env, org_env, bucket_env
 
-
-i = 0
-setup = env_setup()
-port = setup[0]
-token = setup[1]
-bucket = setup[2]
-org = setup[3]
+port, token, org, bucket = env_setup()
 
 ser = serial.Serial(port, 9600, timeout=5)
 time.sleep(2)
 
-while ser.inWaiting() > 0:
-    LINE = str(ser.readline())
-    LINE = LINE.strip("',/\\n")
-    LINE = LINE.strip("',/\\r")
-    LINE = LINE.strip("b")
-    LINE = LINE.strip("'")
-    LINE = LINE.strip("\'")
-    LINE = LINE.split(",")
-
-    print(LINE)
-    with InfluxDBClient(url="http://localhost:8086", token=token,
+with InfluxDBClient(url="http://localhost:8086", token=token,
                         org=org) as client:
-        write_api = client.write_api(write_options=SYNCHRONOUS)
-        write_api.write(
-            bucket, org,
-            Point(LINE[0].strip(" ")).field(LINE[1].strip(" "), int(LINE[2])))
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+
+    while True:
+        while ser.inWaiting() > 0:
+            LINE = str(ser.readline())
+            LINE = LINE.strip("',/\\n")
+            LINE = LINE.strip("',/\\r")
+            LINE = LINE.strip("b")
+            LINE = LINE.strip("'")
+            LINE = LINE.strip("\'")
+            LINE = LINE.split(",")
+
+            if len(LINE) == 3:
+                write_api.write(
+                    bucket, org,
+                    Point(LINE[0]).field(LINE[1], int(LINE[2])))
